6d507b1ef9db9ff086c44090d22e43fc2e84410f
antoinerey1 <antoine.rey1.pro@gmail.com>
2017-12-19 17:41:28 +0100

    ajout de page
    connexion, accueil, inscription (non fini avec bug)

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {SigninPage} from "../signin/signin";
+import {SignupPage} from "../signup/signup";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController) {}
 
+  
+  login() {
+    this.navCtrl.push(SigninPage);
   }
-
+  registerPage() {
+    this.navCtrl.push(SignupPage);
+  }
 }
---
+++ src/pages/signin/signin.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+
+@Component({
+  selector: 'page-signin',
+  templateUrl: 'signin.html'
+})
+
+
+export class SigninPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+}
